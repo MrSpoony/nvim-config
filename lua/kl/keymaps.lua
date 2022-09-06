0ab4fd84e997d19e8f3b8f6356b29c744debe8b6
MrSpoony <kimi.loeffel@hispeed.ch>
2022-09-06 11:17:21 +0200

    center on C-d/u remove oo OO keymaps

--- lua/kl/keymaps.lua
+++ lua/kl/keymaps.lua
@@ -18,15 +18,15 @@ Vnoremap("<Down>", "<Nop>")
 Vnoremap("<Right>", "<Nop>")
 Vnoremap("<Left>", "<Nop>")
 
+-- Center cursor on search or scroll
 Nnoremap("n", "nzzzv")
 Nnoremap("N", "Nzzzv")
+Nnoremap("<C-d>", "<C-d>zz")
+Nnoremap("<C-u>", "<C-u>zz")
 
 Nnoremap("J", "mzJ`z")
 
 Nnoremap("<Esc>", "<cmd>noh<CR>")
-
-Nnoremap("oo", "o<Esc>")
-Nnoremap("OO", "O<Esc>")
 
 Vnoremap("<leader><Up>",   "<cmd>'<,'>m '>+1<CR>gv=gv")
 Vnoremap("<leader><Down>", "<cmd>'<,'>m '<-2<CR>gv=gv")
@@ -38,14 +38,13 @@ Nnoremap("<leader>sp", function() vim.wo.spell = not vim.wo.spell end)
 Nnoremap("<leader>+", "<cmd>vertical resize +5<CR>")
 Nnoremap("<leader>-", "<cmd>vertical resize -5<CR>")
 
-local source = function()
-    Nnoremap("<leader>so", "<cmd>w<CR><cmd>so %<CR>")
-end
-
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
     pattern = {"*.lua", "*.vim"},
-    callback = source
+    callback = function() Nnoremap("<leader>so", "<cmd>w<CR><cmd>so %<CR>") end
 })
+
+Nnoremap("<C-V>", "<cmd>vsplit<CR>")
+Nnoremap("<C-B>", "<cmd>split<CR>")
 
 Nnoremap("<leader>q", "<cmd>bdelete!<CR>")
 Nnoremap("<leader>bq", "<cmd>quit!<CR>")
