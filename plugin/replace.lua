e46c42bcf0e3e35fbc036ce4d09acfd135771810
MrSpoony <kimi.loeffel@hispeed.ch>
2022-09-12 07:58:07 +0200

    substitute substitute plugin for replace plugin

--- plugin/replace.lua
+++ plugin/replace.lua
@@ -1,3 +1,9 @@
-Nnoremap("gr", "<Plug>ReplaceMotion")
-Nnoremap("grr", "<Plug>ReplaceLine")
-Vnoremap("gr", "<Plug>ReplaceVisual")
+local sub = require("substitute")
+
+Nnoremap("s", sub.operator)
+Nnoremap("ss", sub.line)
+Nnoremap("S", sub.eol)
+Xnoremap("s", sub.visual)
+
+Nnoremap("gr", "<cmd>echom 'use `sw`'<cr>")
+Xnoremap("gr", "<cmd>echom 'use `sw`'<cr>")
