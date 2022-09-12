d8d8646b6e105e0b7fbd642a3a8f532818b7b847
MrSpoony <kimi.loeffel@hispeed.ch>
2022-09-12 07:55:53 +0200

    add package `dirname` on new file

--- lua/kl/go.lua
+++ lua/kl/go.lua
@@ -42,3 +42,24 @@ vim.api.nvim_create_autocmd({
         fmt.goimport()
     end
 })
+
+
+vim.api.nvim_create_autocmd({ "BufNewFile" }, {
+    pattern = "*.go",
+    callback = function()
+        local dir = vim.fn.expand("%:p:h")
+        dir = dir:gsub(".*/", "")
+        dir = "package " .. dir
+        vim.fn.append(0, dir)
+    end
+})
+local api = require('nvim-tree.api')
+local Event = require('nvim-tree.api').events.Event
+api.events.subscribe(Event.FileCreated, function(data)
+    if not data.fname:find(".go") then return end
+    local dir = data.fname
+    dir = dir:gsub("/[^/]*$", "")
+    dir = dir:gsub(".*/", "")
+    dir = "package " .. dir .. "\n"
+    vim.fn.system("echo \"" .. dir .. "\" > " .. data.fname)
+end)
