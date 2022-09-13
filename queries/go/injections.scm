e6c337c9d176ba481b0fb696c8197f7275f098ae
MrSpoony <kimi.loeffel@hispeed.ch>
2022-09-13 17:01:54 +0200

    shitty injections finally work without errors

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -1,9 +1,18 @@
 (
   (raw_string_literal) @sql
-  (#lua-match? @sql "--sql")
+  (#contains? @sql "-- sql" "--sql")
+  (#offset! @sql 0 1 0 -1)
 )
-
 (
   (interpreted_string_literal) @sql
-  (#lua-match? @sql "--sql")
+  (#contains? @sql "-- sql" "--sql")
+  (#offset! @sql 0 1 0 -1)
+)
+(
+  (const_spec
+    name: (identifier) @_id
+    value: (expression_list (raw_string_literal) @sql)
+  )
+  (#contains? @_id "Query")
+  (#offset! @sql 0 1 0 -1)
 )
