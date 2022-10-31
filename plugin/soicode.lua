4f41921919eed12282e3de3a970f608091f415f0
MrSpoony <kimi.loeffel@hispeed.ch>
2022-10-31 17:59:23 +0100

    format stoml like toml

--- plugin/soicode.lua
+++ plugin/soicode.lua
@@ -2,3 +2,10 @@ vim.g.soicode_auto_insert_template = true
 vim.g.soicode_no_clangdfile = true
 vim.g.soicode_enable_all_cpp_files = true
 vim.g.soicode_use_predefined_keybindings = true
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    pattern = "*.stoml",
+	callback = function()
+		vim.opt_local.ft = "toml"
+	end
+})
