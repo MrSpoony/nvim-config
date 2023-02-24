3a73ceaf7e120cd8b70799d7f8c949b22d0a3d4d
MrSpoony <kimi.loeffel@hispeed.ch>
2023-02-24 22:54:23 +0100

    make my template small again

--- templates/soi.cpp
+++ templates/soi.cpp
@@ -1,56 +1,17 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-#define FOR(i, j, k, in) for (long long int i = j; i < k; i += in)
-#define REP(i, j) FOR(i, 0, j, 1)
-#define MP make_pair
-#define PB push_back
-#define INF 2147483647
-#define INFINITE 9223372036854775807
-#define MOD 1000000007
-#define dl "\n"
-#define DBG(x) cout << #x << " = " << x << endl;
-
 using ll = long long int;
-using PII = pair<ll, ll>;
-using PQI = priority_queue<ll>;
-using RPQI = priority_queue<ll, vector<ll>, greater<ll>>;
-using PQII = priority_queue<PII>;
-using QI = queue<ll>;
 template <typename T> using V = vector<T>;
-using VB = vector<bool>;
-using VS = vector<string>;
-using VI = vector<ll>;
-using VII = vector<PII>;
-using VVI = vector<VI>;
-using VVII = vector<VII>;
 
-template <typename T1, typename T2>
-istream &operator>>(istream &istream, pair<T1, T2> &p) {
-    return istream >> p.first >> p.second;
-}
-template <typename T> istream &operator>>(istream &istream, vector<T> &v) {
-    for (auto &it : v)
-        cin >> it;
-    return istream;
-}
-template <typename T1, typename T2>
-ostream &operator<<(ostream &ostream, pair<T1, T2> &p) {
-    return ostream << p.first << " " << p.second;
-}
-template <typename T> ostream &operator<<(ostream &ostream, vector<T> &v) {
-    for (auto &it : v)
-        cout << it << " ";
-    return ostream;
-}
-
+#define dl "\n"
+const int MOD = 1000000007;
+const int INF = 2147483647;
+const ll INFINITE = 9223372036854775807;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-
-    
 
     return 0;
 }
