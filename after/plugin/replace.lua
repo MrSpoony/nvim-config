f4973fb118cd77e0d80791547c507d5e9cf214e2
Kimi Loeffel <kimi.loeffel@bit.admin.ch>
2023-04-13 09:05:05 +0200

    disable highlight

--- after/plugin/replace.lua
+++ after/plugin/replace.lua
@@ -1,5 +1,12 @@
 local sub = require("substitute")
 
+sub.setup({
+	highlight_substituted_text = {
+		enabled = false,
+		timer = 500,
+	},
+})
+
 Nnoremap("s", sub.operator)
 Nnoremap("ss", sub.line)
 Nnoremap("S", sub.eol)
