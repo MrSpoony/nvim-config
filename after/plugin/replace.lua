c5bd5726718a1a61235166a9651b1b674469edda
Kimi Loeffel <kimi.loeffel@bit.admin.ch>
2023-08-10 12:56:30 +0200

    add + register substitute commadns

--- after/plugin/replace.lua
+++ after/plugin/replace.lua
@@ -11,3 +11,8 @@ Nnoremap("s", sub.operator)
 Nnoremap("ss", sub.line)
 Nnoremap("S", sub.eol)
 Xnoremap("s", sub.visual)
+
+Nnoremap("<leader>s", Fn(sub.operator), { register = "+" })
+Nnoremap("<leader>ss", Fn(sub.line), { register = "+" })
+Nnoremap("<leader>S", Fn(sub.eol), { register = "+" })
+Xnoremap("<leader>s", Fn(sub.visual), { register = "+" })
